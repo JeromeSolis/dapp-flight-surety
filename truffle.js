b8a3755fa4b504d0da8b669f5f6b8acfc9d64b93
Jerome Solis <jay@Js-MacBook-Pro.local>
2021-09-12 17:57:54 -0400

    First commit

--- truffle.js
+++ truffle.js
@@ -1,19 +1,19 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+var mnemonic = "tennis notice reflect salon royal rhythm rich supply culture isolate century dog";
 
 module.exports = {
   networks: {
     development: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 50);
       },
       network_id: '*',
-      gas: 9999999
+      // gas: 9999999
     }
   },
   compilers: {
     solc: {
-      version: "^0.4.24"
+      version: "^0.4.25"
     }
   }
 };
